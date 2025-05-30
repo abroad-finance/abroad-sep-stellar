541261a089f6e0a8a9a77d6728feaf3740899fb3
Hollwann <hollwann@gmail.com>
2025-05-30 15:46:03 +0000

    Update DATABASES setting to check for DATABASE_URL instead of DJANGO_DATABASE_URL

--- abroad/settings.py
+++ abroad/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "abroad.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get("DJANGO_DATABASE_URL") is None:
+if os.environ.get("DATABASE_URL") is None:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
