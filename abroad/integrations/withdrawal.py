cafba804a051245ef361d0cead404d1679823575
Hollwann <hollwann@gmail.com>
2025-05-30 14:30:30 +0000

    Refactor interactive_url method to use environment variable for base URL and remove unused methods

--- abroad/integrations/withdrawal.py
+++ abroad/integrations/withdrawal.py
@@ -1,5 +1,6 @@
 from typing import Dict, Optional, List
 from decimal import Decimal
+import os
 
 from django import forms
 from django.http import QueryDict
@@ -37,7 +38,8 @@ class WithdrawalAbroad(WithdrawalIntegration):
             withdrawal flow
         """
         print("request", request)
-        return "http://localhost:5173/mobile/anchor"
+        base_url = os.environ.get("INTERACTIVE_URL_BASE", "http://localhost:5173")
+        return f"{base_url}/mobile/anchor?transaction_id={transaction.id}&asset_code={asset.code}&callback={callback}&lang={lang}"
 
     def after_interactive_flow(self, request: Request, transaction: Transaction):
         """
@@ -46,60 +48,6 @@ class WithdrawalAbroad(WithdrawalIntegration):
         transaction.amount_expected = Decimal(request.query_params.get("amount_expected"))
         transaction.status = Transaction.STATUS.pending_user_transfer_start
         transaction.save()
-
-    def save_sep9_fields(
-        self,
-        token: SEP10Token,
-        request: Request,
-        stellar_account: str,
-        fields: Dict,
-        language_code: str,
-        muxed_account: Optional[str] = None,
-        account_memo: Optional[str] = None,
-        account_memo_type: Optional[str] = None,
-        *args: List,
-        **kwargs: Dict
-    ):
-        """
-        Same as ``DepositIntegration.save_sep9_fields``
-        """
-        raise NotImplementedError()
-
-    def process_sep6_request(
-        self,
-        token: SEP10Token,
-        request: Request,
-        params: Dict,
-        transaction: Transaction,
-        *args: List,
-        **kwargs: Dict
-    ) -> Dict:
-        """
-        .. _/withdraw: https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0006.md#withdraw
-        .. _Withdraw no additional information needed: https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0006.md#1-success-no-additional-information-needed-1
-
-        Same as ``DepositIntegration.process_sep6_request`` except for the case below.
-        Specifically, the ``how`` attribute should not be included.
-
-        `Withdraw no additional information needed`_
-
-        A success response. Polaris populates most of the attributes for this response.
-        Simply return an 'extra_info' attribute if applicable:
-        ::
-
-            {
-                "extra_info": {
-                    "message": "Send the funds to the following stellar account including 'memo'"
-                }
-            }
-
-        In addition to this response, you may also return the `Customer information needed`_
-        and `Customer Information Status`_ responses as described in
-        ``DepositIntegration.process_sep6_request``.
-        """
-        raise NotImplementedError(
-            "`process_sep6_request` must be implemented if SEP-6 is active"
-        )
 
     def patch_transaction(
         self,
