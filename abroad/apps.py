baceb8dcc7f2f493c04c529d87ea3d38c1eea420
Hollwann <hollwann@gmail.com>
2025-07-17 05:30:11 +0000

    Remove unused QuoteIntegration import from AbroadConfig

--- abroad/apps.py
+++ abroad/apps.py
@@ -7,11 +7,9 @@ class AbroadConfig(AppConfig):
     def ready(self):
         from polaris.integrations import register_integrations
         from .sep1 import return_toml_contents
-        from .integrations.quote import QuoteIntegration
         from .integrations.withdrawal import WithdrawalAbroad
 
         register_integrations(
             toml=return_toml_contents,
-            quote=QuoteIntegration(),
             withdrawal=WithdrawalAbroad(),
         )
